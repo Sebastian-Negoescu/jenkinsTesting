ba05ced7283dbba60734ea8f3b49b639fd1eafc2
Negoescu <s.negoescu@accenture.com>
2019-10-11 13:49:01 +0300

    no gitignore

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"";
+            StreamReader file = new StreamReader(filename);
+            string line;
+
+            while ((line = file.Read()) != null) {
+                Console.WriteLine($"{line.ToUpper()}");
+            }
+            Console.WriteLine("End of file...");
+        }
+    }
+}
